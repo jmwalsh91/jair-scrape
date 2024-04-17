fd013892b3bcd5a3b4a05e085715c4535b8c592e
jmwalsh91 <jordan@jmwalsh.dev>
2024-04-17 17:06:21 -0400

    now correctly gets pdf from viewer

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.22.0
 
 require github.com/PuerkitoBio/goquery v1.9.1
 
+require golang.org/x/sys v0.17.0 // indirect
+
 require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/net v0.21.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,28 +5,43 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/sirupsen/logrus"
 )
 
-const baseIssueID = 1085
+const baseIssueURL = "https://www.jair.org/index.php/jair/issue/view/"
 
-func generateIssueURL(issueID int) string {
-	return fmt.Sprintf("https://www.jair.org/index.php/jair/issue/view/%d", issueID)
+var log = logrus.New()
+
+func setupLogger() {
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		ForceColors:   true,
+	})
+	log.SetLevel(logrus.InfoLevel)
 }
 
-func findPDFLinks(issueURL string) ([]string, []string, error) {
+func generateIssueURL(issueID int) string {
+	return fmt.Sprintf("%s%d", baseIssueURL, issueID)
+}
+
+func findPDFViewerLinks(issueURL string) ([]string, []string, error) {
 	resp, err := http.Get(issueURL)
 	if err != nil {
+		log.WithField("url", issueURL).Error("Failed to fetch issue page:", err)
 		return nil, nil, err
 	}
 	defer resp.Body.Close()
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
+		log.WithField("url", issueURL).Error("Failed to parse HTML document:", err)
 		return nil, nil, err
 	}
 
@@ -34,9 +49,10 @@ func findPDFLinks(issueURL string) ([]string, []string, error) {
 	var titles []string
 	doc.Find(".article-summary .media-body").Each(func(i int, s *goquery.Selection) {
 		title := s.Find("h3.media-heading a").Text()
-		pdfLink, exists := s.Find(".btn-group a.pdf").Attr("href")
+		viewerLink, exists := s.Find(".btn-group a.pdf").Attr("href")
 		if exists {
-			links = append(links, pdfLink)
+			fullLink := resolveURL(viewerLink)
+			links = append(links, fullLink)
 			titles = append(titles, strings.TrimSpace(title))
 		}
 	})
@@ -44,57 +60,107 @@ func findPDFLinks(issueURL string) ([]string, []string, error) {
 	return links, titles, nil
 }
 
+func resolveURL(href string) string {
+	if strings.HasPrefix(href, "http") {
+		return href
+	}
+	return "https://www.jair.org" + href
+}
+
+func extractActualPDFLink(pageURL string) (string, error) {
+	resp, err := http.Get(pageURL)
+	if err != nil {
+		log.WithField("url", pageURL).Error("Failed to fetch viewer page:", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.WithField("url", pageURL).Error("Failed to read response body:", err)
+		return "", err
+	}
+	bodyString := string(bodyBytes)
+
+	re := regexp.MustCompile(`var pdfUrl = "([^"]+)"`)
+	matches := re.FindStringSubmatch(bodyString)
+	if len(matches) < 2 {
+		log.WithField("url", pageURL).Error("No PDF URL found in script")
+		return "", fmt.Errorf("no PDF URL found in script")
+	}
+
+	decodedURL, err := url.QueryUnescape(matches[1])
+	if err != nil {
+		log.WithField("encoded URL", matches[1]).Error("Failed to decode URL:", err)
+		return "", err
+	}
+
+	decodedURL = strings.ReplaceAll(decodedURL, `\/`, `/`)
+	return decodedURL, nil
+}
+
 func downloadPDF(pdfURL, filePath string) error {
 	resp, err := http.Get(pdfURL)
 	if err != nil {
+		log.WithField("PDF URL", pdfURL).Error("Failed to download PDF:", err)
 		return err
 	}
 	defer resp.Body.Close()
 
 	out, err := os.Create(filePath)
 	if err != nil {
+		log.WithField("file path", filePath).Error("Failed to create file:", err)
 		return err
 	}
 	defer out.Close()
 
 	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		log.WithField("file path", filePath).Error("Failed to write PDF:", err)
+	}
+	log.WithField("file path", filePath).Info("PDF downloaded successfully")
 	return err
 }
 
 func main() {
+	setupLogger()
 	outputDir := flag.String("output", ".", "Directory to save downloaded PDFs")
-	//Ids on the website begin at 1085 for issue 1.
-	endIssueCount := flag.Int("endIssue", 75, "The number of issues to include.")
+	endIssueCount := flag.Int("endIssue", 75, "The number of issues to include from 1, starting id is 1085")
 	flag.Parse()
 
 	if err := os.MkdirAll(*outputDir, 0755); err != nil {
-		fmt.Println("Failed to create output directory:", err)
+		log.Error("Failed to create output directory:", err)
 		return
 	}
 
-	finalIssueID := baseIssueID + *endIssueCount - 1
+	finalIssueID := 1085 + *endIssueCount - 1
 
-	for issueID := baseIssueID; issueID <= finalIssueID; issueID++ {
+	for issueID := 1085; issueID <= finalIssueID; issueID++ {
 		issueURL := generateIssueURL(issueID)
-		fmt.Println("Processing:", issueURL)
-		pdfLinks, titles, err := findPDFLinks(issueURL)
+		log.Info("Processing issue:", issueURL)
+		viewerLinks, titles, err := findPDFViewerLinks(issueURL)
 		if err != nil {
-			fmt.Println("Failed to find PDF links for issue", issueID, ":", err)
+			log.WithField("issue", issueID).Error("Failed to find viewer links:", err)
 			continue
 		}
 
-		for j, link := range pdfLinks {
+		for j, link := range viewerLinks {
 			title := titles[j]
-			fmt.Println("Downloading PDF from:", link, "Title:", title)
-			safeTitle := strings.ReplaceAll(title, " ", "_")
-			fileName := fmt.Sprintf("%s.pdf", safeTitle)
-			filePath := filepath.Join(*outputDir, fileName)
-			err = downloadPDF(link, filePath)
+			log.Info("Found PDF viewer link:", link)
+			actualPDFLink, err := extractActualPDFLink(link)
 			if err != nil {
-				fmt.Println("Failed to download PDF from", link, ":", err)
+				log.WithField("title", title).Error("Failed to extract PDF link from viewer:", err)
 				continue
 			}
-			fmt.Println("Successfully downloaded:", filePath)
+
+			fileName := fmt.Sprintf("%s.pdf", title)
+			filePath := filepath.Join(*outputDir, fileName)
+			err = downloadPDF(actualPDFLink, filePath)
+			if err != nil {
+				log.WithField("title", title).Error("Failed to download PDF:", err)
+				continue
+			}
+			log.WithField("title", title).Info("Successfully downloaded:", filePath)
 		}
 	}
 }
